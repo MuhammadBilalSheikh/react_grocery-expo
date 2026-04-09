05dd45ff077a9b6e5c8c7d0b88d7400522caafe7
MD Bilal <mbs9246@gmail.com>
2026-04-09 16:02:29 +0500

    feat: add grocery item management with database integration
    - Updated package.json to include new dependencies for Neon database and Drizzle ORM.
    - Created seed script to populate grocery items in the database.
    - Implemented database actions for listing, creating, updating, and deleting grocery items.
    - Established database client connection using Neon serverless.
    - Defined schema for grocery items in the database.

---
+++ src/lib/server/db/client.ts
@@ -0,0 +1,15 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+
+ 
+
+const databaseURl = process.env.DATABASE_URL as string;
+
+if (!databaseURl) {
+    throw new Error("DATABASE_URL is not defined in environment variables / API route");
+}
+
+const sql = neon(databaseURl);
+export const db = drizzle({client: sql, schema});
